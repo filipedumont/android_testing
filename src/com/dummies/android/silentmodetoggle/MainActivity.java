4e037b80cc09fba36626af11fa4040fd730250e4
Filipe Franca <filipedumont@gmail.com>
2014-03-16 14:57:02 -0300

    dummies book so far

---
+++ src/com/dummies/android/silentmodetoggle/MainActivity.java
@@ -0,0 +1,38 @@
+package com.dummies.android.silentmodetoggle;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		setButtonClickListener();
+	}
+	
+	private void setButtonClickListener(){
+		Button toggleButton = (Button)findViewById(R.id.toggleButton);
+		toggleButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
